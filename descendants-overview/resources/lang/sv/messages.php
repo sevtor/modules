9cc68154dae7110885a3589c1a15085b43244146
Seved Torstendahl <seved@torstendahl.se>
2024-02-11 13:16:56 +0100

    New files messages.pot are added that shows the current phrases that need translation, can be used as e.g. msgmerge -U sv/messages messages.pot The swedish translation files are updated.

--- descendants-overview/resources/lang/sv/messages.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array (
-  'An overview of the descendants of an individual.' => 'En översikt av ättlingarna till en person.',
-  'Descendants overview' => 'Ättlingar översikt',
-  'emig' => 'emig',
-  'Married within this tree, see above!' => 'Gift inom detta träd, se ovan!',
-  'sp' => 'ma',
-);
--- family-individuals/resources/lang/sv/messages.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array (
-  'A report of an individual’s relatives, one per page.' => 'En rapport om en persons släktingar, en per sida.',
-  'Family individuals' => 'Släktingar',
-  'generation' => 'generation',
-  'née' => 'f.',
-  'Death of spouse' => 'Makes/makas död',
-);
